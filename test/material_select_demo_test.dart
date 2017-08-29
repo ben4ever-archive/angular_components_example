0f6763efbce9b0c77160247a4371be2d33218e00
Benjamin Dopplinger <b.dopplinger@gmail.com>
2017-08-29 13:16:19 +1000

    Avoid having to wrap `MaterialSelectDemoComponent`
    This finally enables me to effeciently set up the test environment
    without having to wrap the tested `Component` (as it was done so far).
    `NgTestBed.setPageLoader(...)` is used now to set the `PageLoader`
    parent element to a container which contains `NgTestBed`'s host element
    and `overlayContainerParent`'s element.
    
    At last, after 3 weeks of research, this finally works as I envisioned
    it in the beginning - Soli Deo Gloria!!

--- test/material_select_demo_test.dart
+++ test/material_select_demo_test.dart
@@ -7,20 +7,39 @@ import 'dart:html';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_test/angular_test.dart';
 import 'package:angular/angular.dart';
+import 'package:pageloader/html.dart';
 import 'package:test/test.dart';
 
 import 'package:angular_components_example/src/material_select_demo/material_select_demo.dart';
 import 'material_select_demo_po.dart';
 
+PageLoader Function(
+        Element el, NgTestFixture<MaterialSelectDemoComponent> fixture)
+    _getCreatePageLoaderFunc(Element el) {
+  return (_, fixture) => new HtmlPageLoader(el, executeSyncedFn: (fn) async {
+        await fn();
+        return fixture.update();
+      });
+}
+
 @AngularEntrypoint()
 void main() {
-  NgTestFixture<ParentComponent> fixture;
+  NgTestFixture<MaterialSelectDemoComponent> fixture;
   MaterialSelectDemoPO po;
 
   tearDown(disposeAnyRunningTest);
 
   setUp(() async {
-    final testBed = new NgTestBed<ParentComponent>();
+    var parentDiv = new DivElement(),
+        hostDiv = new DivElement(),
+        overlayContDiv = new DivElement();
+    parentDiv.children..add(hostDiv)..add(overlayContDiv);
+    final testBed = new NgTestBed<MaterialSelectDemoComponent>(host: hostDiv)
+        .setPageLoader(_getCreatePageLoaderFunc(parentDiv))
+        .addProviders([
+      materialProviders,
+      provide(overlayContainerParent, useValue: overlayContDiv),
+    ]);
     fixture = await testBed.create();
     po = await fixture.resolvePageObject(MaterialSelectDemoPO);
     await fixture.update();
@@ -51,19 +70,3 @@ void main() {
     });
   });
 }
-
-HtmlElement getOverlayContainerParent(Element parent) => parent;
-
-@Component(
-  selector: 'parent',
-  template: '<material-select-demo></material-select-demo>',
-  directives: const [
-    MaterialSelectDemoComponent,
-  ],
-  providers: const [
-    materialProviders,
-    const Provider(overlayContainerParent,
-        useFactory: getOverlayContainerParent, deps: const [Element]),
-  ],
-)
-class ParentComponent {}
